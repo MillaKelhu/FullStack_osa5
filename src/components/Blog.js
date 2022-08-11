7c75d6e1725970ba8d9ac39dbcb625fb2858d426
Milla Kelhu <milla.kelhu@gmail.com>
2022-08-11 14:22:26 +0300

    Exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,42 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState, useEffect } from 'react'
+
+const Blog = ({blog}) => {
+  const [blogInfoVisible, setBlogInfoVisible] = useState(false)
+
+  const handleLike = () => {
+    console.log(`${blog.title} like pressed`)
+  }
+
+  const hideWhenVisible = {display: blogInfoVisible ? 'none' : ''}
+  const showWhenVisible = {display: blogInfoVisible ? '' : 'none'}
+
+  return (
+    <div className='blog'>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={() => setBlogInfoVisible(true)}>
+          view
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={() => setBlogInfoVisible(false)}>
+            hide
+          </button>
+        </div>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes {blog.likes}
+          <button onClick={handleLike}>
+            like
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
