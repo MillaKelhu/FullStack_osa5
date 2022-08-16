1e60d78595c4c1ea6ab6315581d23d9f2a6c2307
Milla Kelhu <milla.kelhu@gmail.com>
2022-08-16 16:27:00 +0300

    Exercise 5.18

--- cypress/e2e/blog_app.spec.cy.js
+++ cypress/e2e/blog_app.spec.cy.js
@@ -1,13 +1,62 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const newUser = {
+      username: 'user',
+      name: 'Test User',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', newUser)
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
-    cy.get('h2').contains('Log in to application')
+    cy.get('h2')
+      .contains('Log in to application')
+
     cy.contains('username')
+
     cy.contains('password')
-    cy.get('button').contains('login')
+
+    cy.get('button')
+      .contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input[name="Username"]')
+        .type('user')
+
+      cy.get('input[name="Password"]')
+        .type('password')
+
+      cy.get('button')
+        .contains('login')
+        .click()
+
+      cy.get('h2')
+        .contains('blogs')
+
+      cy.contains('Test User logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input[name="Username"]')
+        .type('user')
+
+      cy.get('input[name="Password"]')
+        .type('notmypassword')
+
+      cy.get('button')
+        .contains('login')
+        .click()
+
+      cy.get('h2')
+        .contains('Log in to application')
+
+      cy.get('div.error')
+        .contains('wrong username or password')
+        .should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
